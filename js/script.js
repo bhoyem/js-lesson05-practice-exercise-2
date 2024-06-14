72aba35fbe0fd878e154518813103d0f0f484b7f
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-06-14 12:49:27 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var fans = document.querySelector(".fans");
+// console.log(fans.innerText);
+fans.addEventListener("mouseover", function () {
+  fans.innerText = "123k";
+});
+
+var fish = document.querySelector(".fish");
+// console.log(fish.outerHTML);
+fish.addEventListener("mouseover", function () {
+  fish.innerText = "47k";
+});
+var paw = document.querySelector(".pets");
+// console.log(paw.outerHTML);
+paw.addEventListener("mouseover", function () {
+  paw.innerText = "20k";
+});
